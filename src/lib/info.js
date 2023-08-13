185561b31e9ef5e797c9d7fb21bb73101a6035b5
soulwax <soulwax-95@protonmail.com>
2023-08-13 14:17:50 +0000

    initial commit

---
+++ src/lib/info.js
@@ -0,0 +1,29 @@
+// This is for convenience as you start out with this template.
+// If you'd rather hardcode these values into your pages,
+// feel free to remove this file.
+
+// URL of your website
+export const website = 'https://sveltekit-blog-template.vercel.app'
+
+export const firstName = ''
+export const lastName = 'Kling'
+export const name = firstName && lastName ? `${firstName} ${lastName}` : firstName || lastName
+
+// Used for the landing page & footer of posts
+export const avatar = 'https://100k-faces.glitch.me/random-image'
+export const bio = `\
+I'm ${name}, a software engineer and neurology student based in Germany. \
+I write about software development, productivity, and health. \
+I'm passionate about combining technology with neuroscience to create innovative solutions for medical challenges. \
+In my free time, I enjoy reading about the latest advancements in AI and attending tech conferences across Europe. \
+When I'm not coding or studying, you can find me trailblazing into the unknown. \
+I believe in lifelong learning and am always eager to collaborate with like-minded individuals on exciting projects.
+`
+
+// Your social usernames, empty strings won't be used.
+// For sake of the live demo, I'm using `username/repo` for github,
+// but you can replace with just your username.
+export const github = 'soulwax/tetsu-blog-engine'
+export const twitter = ''
+export const linkedin = ''
+export const instagram = ''
